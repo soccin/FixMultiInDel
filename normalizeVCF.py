1b78e7a55cd72a0ba8246c447a5d0d450ccf6e65
Nicholas D. Socci <soccin@mskcc.org>
2016-12-09 03:55:58 -0500

    Deletion normalization finished

--- normalizeVCF.py
+++ normalizeVCF.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 import sys
+import csv
 from VCFGenerator import VCFGenerator
 
 fp=open(sys.argv[1])
@@ -8,6 +9,12 @@ fp=open(sys.argv[1])
 vcf=VCFGenerator(fp)
 
 #lineterminator="\n"
+
+cout=csv.DictWriter(
+	sys.stdout,
+	vcf.cHeader,
+	delimiter="\t",
+	lineterminator="\n")
 
 for v in vcf:
 	ref=v["REF"]
@@ -24,6 +31,11 @@ for v in vcf:
 				print " "*(delta+1),alt[delta:]
 				print v["CHROM"],pos+delta,ref[delta:],alt[delta:]
 				print
+
+				v["POS"]=pos+delta
+				v["REF"]=ref[delta:]
+				v["ALT"]=alt[delta:]
+
 			elif ref.endswith(alt[1:]):
 				delta=len(ref)-len(alt)+1
 				print ".",ref[:delta]
@@ -31,6 +43,14 @@ for v in vcf:
 				print v["CHROM"],pos,ref[:delta],alt[0]
 				print
 
+				v["REF"]=ref[:delta]
+				v["ALT"]=alt[0]
+
 			else:
 				print >>sys.stderr,"ComplexDeletion", ref, ">", alt, 
 				raise ValueError("ComplexDEL %s:%s" %(v["CHROM"],pos))
+
+			v["ID"]="DEL"
+			print v["CHROM"],v["POS"],v["REF"],v["ALT"],"FIX"
+
+	cout.writerow(v)
