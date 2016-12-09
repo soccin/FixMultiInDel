2f847e1087bad154f093664e33273073edbcfadc
Nicholas D. Socci <soccin@mskcc.org>
2016-12-09 17:41:20 -0500

    Added insertion processing

--- normalizeVCF.py
+++ normalizeVCF.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python2.7
 
+"""
+normalizeInDels
+
+Given a VCF in which multi-allelic events have already been
+split this script normalize any _padded_ events, i.e.;
+
+Deletion:
+	chr1,970549,TGG,TG
+	chr1,970550, GG, G
+
+Insertion:
+	chr1,4439107,AAGGAGG,AAGGAGGAGG
+	chr1,4439113,      G,      GAGG
+
+Since this is for the post processing of in/dels from
+haplotypecaller only in/dels are writing out, SNPs (and ONPs)
+are filtered out to reduce file size.
+
+"""
+
 import sys
 import csv
 from VCFGenerator import VCFGenerator
 
-fp=open(sys.argv[1])
+if len(sys.argv)==2:
+	fp=open(sys.argv[1])
+else:
+	fp=sys.stdin
 
 vcf=VCFGenerator(fp)
 
@@ -20,9 +43,8 @@ for v in vcf:
 	alt=v["ALT"]
 	pos=int(v["POS"])
 	if len(ref)>1 and len(alt)>1:
-		print v["#CHROM"],v["POS"],v["REF"],v["ALT"]
 		if len(ref)>len(alt):
-			
+
 			# Deletion
 			if ref.startswith(alt):
 				delta=len(alt)-1
@@ -36,15 +58,30 @@ for v in vcf:
 				v["ALT"]=alt[0]
 
 			else:
-				print >>sys.stderr,"ComplexDeletion", ref, ">", alt, 
-				raise ValueError("ComplexDEL %s:%s" %(v["CHROM"],pos))
-
-			v["ID"]="DEL"
+				print >>sys.stderr,"ComplexDeletion", ref, ">", alt
+				raise ValueError("ComplexDEL %s:%s" %(v["#CHROM"],pos))
 
 		elif len(ref)<len(alt):
-			v["ID"]="INS"
 
-		else:
-			v["ID"]="ONP"
+			# Insertion
+			if alt.startswith(ref):
+				delta=len(ref)-1
+				v["POS"]=pos+delta
+				v["REF"]=ref[delta:]
+				v["ALT"]=alt[delta:]
 
-	cout.writerow(v)
+			elif alt.endswith(ref[1:]):
+				delta=len(alt)-len(ref)+1
+				v["REF"]=ref[0]
+				v["ALT"]=alt[:delta]
+
+			else:
+				print >>sys.stderr,"ComplexInsertion", ref, ">", alt
+				raise ValueError("ComplexINS %s:%s" %(v["#CHROM"],pos))
+
+
+	if len(ref)!=len(alt):
+		cout.writerow(v)
+
+
+
