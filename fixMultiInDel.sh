87550e9aef1ac679e517e4c2d668c424ca251762
Nicholas D. Socci <soccin@mskcc.org>
2016-12-10 16:26:51 -0500

    Added option for name output file

--- fixMultiInDel.sh
+++ fixMultiInDel.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-HVCF=$1
+case $# in
+    2)
+    HVCF=$1
+    OUTFILE=$2
+    ;;
+
+    1)
+    HCF=$1
+    OUTFILE=$(basename $HVCF | sed 's/.vcf$/___FixInDels.vcf/')
+    ;;
+
+    *)
+    echo
+    echo "usage: fixMultiInDel.sh INPUT_VCF [OUTPUT_VCF]"
+    echo
+    exit
+    ;;
+
+esac
 
 $SDIR/bin/vcfbreakmulti $HVCF \
     | $SDIR/bin/normalizeInDels.py \
-    > $(basename $HVCF | sed 's/.vcf$/___FixInDels.vcf/')
+    > $OUTFILE
 
