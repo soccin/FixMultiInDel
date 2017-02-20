4378ed1de2d87b6a1f51d174e0b41fa20da274ec
Krista Kazmierkiewicz <kristakaz@gmail.com>
2017-02-20 16:44:19 -0500

    updated test info; updated version number

--- test/runTests.sh
+++ test/runTests.sh
@@ -10,12 +10,17 @@
 #$ zcat Proj_06721_C_HaplotypeCaller__FixMulti___v2.0.1.vcf.gz | md5sum -
 #    9c24d5ca1f6bed02bf4c9e79cdf703b5  -
 
+# Newest commit 
+# zcat files/Proj_06721_C_HaplotypeCaller__FixMulti___current.vcf.gz | md5sum -
+#    7cc0cf33e1c0f60abcd1cb7b2bfe2769  -
+
+
 ../fixMultiInDel.sh \
     <(zcat files/Proj_06721_C_HaplotypeCaller__MultiOnly.vcf.gz ) \
     Proj_06721_C_HaplotypeCaller__FixMulti.vcf 2> stderr.log
 
 assert \
     "md5sum Proj_06721_C_HaplotypeCaller__FixMulti.vcf | awk '{print \$1}'" \
-    9c24d5ca1f6bed02bf4c9e79cdf703b5
+    7cc0cf33e1c0f60abcd1cb7b2bfe2769
 
 assert_end
